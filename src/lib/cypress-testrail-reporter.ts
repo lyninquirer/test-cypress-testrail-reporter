292d7f2d889f674f418c7973479a240cd667f5d9
Viet Nguyen Quoc <vnguyenquoc@anevia.com>
2020-04-10 17:05:38 +0700

    Format the code

--- src/lib/cypress-testrail-reporter.ts
+++ src/lib/cypress-testrail-reporter.ts
@@ -6,86 +6,86 @@ import { Status, TestRailResult, Emitted_Events } from './testrail.interface';
 const chalk = require('chalk');
 
 export class CypressTestRailReporter extends reporters.Spec {
-  private results: TestRailResult[] = [];
-  private testRail: TestRail;
+    private results: TestRailResult[] = [];
+    private testRail: TestRail;
 
-  constructor(runner: any, options: any) {
-    super(runner);
+    constructor(runner: any, options: any) {
+        super(runner);
 
-    let reporterOptions = options.reporterOptions;
-    this.testRail = new TestRail(reporterOptions);
-    this.validate(reporterOptions, 'domain');
-    this.validate(reporterOptions, 'username');
-    this.validate(reporterOptions, 'password');
-    this.validate(reporterOptions, 'projectId');
-    this.validate(reporterOptions, 'createTestRun');
-    let event = reporterOptions.event || Emitted_Events.EVENT_RUN_END;
-    if (!Object.values(Emitted_Events).includes(event)) {
-      throw new Error('Value of emitted event is not correct in cypress.json');
-    }
+        let reporterOptions = options.reporterOptions;
+        this.testRail = new TestRail(reporterOptions);
+        this.validate(reporterOptions, 'domain');
+        this.validate(reporterOptions, 'username');
+        this.validate(reporterOptions, 'password');
+        this.validate(reporterOptions, 'projectId');
+        this.validate(reporterOptions, 'createTestRun');
+        let event = reporterOptions.event || Emitted_Events.EVENT_RUN_END;
+        if (!Object.values(Emitted_Events).includes(event)) {
+            throw new Error('Value of emitted event is not correct in cypress.json');
+        }
 
-    runner.on('start', () => {
-      const executionDateTime = moment().format('MMM Do YYYY, HH:mm (Z)');
-      const name = `${reporterOptions.runName || 'Automated test run'} ${executionDateTime}`;
-      const description = '';
+        runner.on('start', () => {
+            const executionDateTime = moment().format('MMM Do YYYY, HH:mm (Z)');
+            const name = `${reporterOptions.runName || 'Automated test run'} ${executionDateTime}`;
+            const description = '';
 
-      if (reporterOptions.createTestRun == "true") {
-        this.testRail.createRun(name, description);
-      }
-    });
-
-    runner.on('pass', test => {
-      const caseIds = titleToCaseIds(test.title);
-      if (caseIds.length > 0) {
-        const results = caseIds.map(caseId => {
-          return {
-            case_id: caseId,
-            status_id: Status.Passed,
-            comment: `Execution time: ${test.duration}ms`,
-          };
+            if (reporterOptions.createTestRun == "true") {
+                this.testRail.createRun(name, description);
+            }
         });
-        this.results.push(...results);
-      }
-    });
 
-    runner.on('fail', test => {
-      const caseIds = titleToCaseIds(test.title);
-      if (caseIds.length > 0) {
-        const results = caseIds.map(caseId => {
-          return {
-            case_id: caseId,
-            status_id: Status.Failed,
-            comment: `${test.err.message}`,
-          };
+        runner.on('pass', test => {
+            const caseIds = titleToCaseIds(test.title);
+            if (caseIds.length > 0) {
+                const results = caseIds.map(caseId => {
+                    return {
+                        case_id: caseId,
+                        status_id: Status.Passed,
+                        comment: `Execution time: ${test.duration}ms`,
+                    };
+                });
+                this.results.push(...results);
+            }
         });
-        this.results.push(...results);
-      }
-    });
 
-    runner.on(event, () => {
-      if (this.results.length == 0) {
-        console.log('\n', chalk.magenta.underline.bold('(TestRail Reporter)'));
-        console.warn(
-          '\n',
-          'No testcases were matched. Ensure that your tests are declared correctly and matches Cxxx',
-          '\n'
-        );
-        this.testRail.deleteRun();
+        runner.on('fail', test => {
+            const caseIds = titleToCaseIds(test.title);
+            if (caseIds.length > 0) {
+                const results = caseIds.map(caseId => {
+                    return {
+                        case_id: caseId,
+                        status_id: Status.Failed,
+                        comment: `${test.err.message}`,
+                    };
+                });
+                this.results.push(...results);
+            }
+        });
 
-        return;
-      }
+        runner.on(event, () => {
+            if (this.results.length == 0) {
+                console.log('\n', chalk.magenta.underline.bold('(TestRail Reporter)'));
+                console.warn(
+                    '\n',
+                    'No testcases were matched. Ensure that your tests are declared correctly and matches Cxxx',
+                    '\n'
+                );
+                this.testRail.deleteRun();
 
-      this.testRail.publishResults(this.results);
-      this.results = []
-    });
-  }
+                return;
+            }
 
-  private validate(options, name: string) {
-    if (options == null) {
-      throw new Error('Missing reporterOptions in cypress.json');
+            this.testRail.publishResults(this.results);
+            this.results = []
+        });
     }
-    if (options[name] == null) {
-      throw new Error(`Missing ${name} value. Please update reporterOptions in cypress.json`);
+
+    private validate(options, name: string) {
+        if (options == null) {
+            throw new Error('Missing reporterOptions in cypress.json');
+        }
+        if (options[name] == null) {
+            throw new Error(`Missing ${name} value. Please update reporterOptions in cypress.json`);
+        }
     }
-  }
 }
--- src/lib/testrail.ts
+++ src/lib/testrail.ts
@@ -4,88 +4,87 @@ const deasync = require('deasync');
 import { TestRailOptions, TestRailResult } from './testrail.interface';
 
 export class TestRail {
-  private base: String;
-  private runId: Number;
-  private res;
+    private base: String;
+    private runId: Number;
+    private res;
 
-  constructor(private options: TestRailOptions) {
-    this.base = `https://${options.domain}/index.php?/api/v2`;
-    this.res = undefined;
-  }
-
-  public createRun(name: string, description: string) {
-    axios({
-      method: 'post',
-      url: `${this.base}/add_run/${this.options.projectId}`,
-      headers: { 'Content-Type': 'application/json' },
-      auth: {
-        username: this.options.username,
-        password: this.options.password,
-      },
-      data: JSON.stringify({
-        suite_id: this.options.suiteId,
-        name,
-        description,
-        include_all: true,
-      }),
-    })
-      .then(response => {
-        console.log('Creating test run... ---> run id is:  ', response.data.id);
-        this.runId = response.data.id;
-      })
-      .catch(error => console.error(error));
-  }
-
-  public deleteRun() {
-    axios({
-      method: 'post',
-      url: `${this.base}/delete_run/${this.runId}`,
-      headers: { 'Content-Type': 'application/json' },
-      auth: {
-        username: this.options.username,
-        password: this.options.password,
-      },
-    }).catch(error => console.error(error));
-  }
-
-  private waitResponse(delay) {
-    if (typeof this.res === "undefined" && delay > 0) {
-        deasync.sleep(1000)
-        this.waitResponse(delay - 1000)
-    }
-  }
-
-  public publishResults(results: TestRailResult[]) {
-
-    if (this.options.createTestRun == "false") {
-      this.runId = this.options.runId;
+    constructor(private options: TestRailOptions) {
+        this.base = `https://${options.domain}/index.php?/api/v2`;
+        this.res = undefined;
     }
 
-    if (typeof this.runId === "undefined") {
-      console.error("runId is undefined.")
-      return
+    public createRun(name: string, description: string) {
+        axios({
+            method: 'post',
+            url: `${this.base}/add_run/${this.options.projectId}`,
+            headers: { 'Content-Type': 'application/json' },
+            auth: {
+                username: this.options.username,
+                password: this.options.password,
+            },
+            data: JSON.stringify({
+                suite_id: this.options.suiteId,
+                name,
+                description,
+                include_all: true,
+            }),
+        })
+            .then(response => {
+                console.log('Creating test run... ---> run id is:  ', response.data.id);
+                this.runId = response.data.id;
+            })
+            .catch(error => console.error(error));
     }
 
-    axios({
-      method: 'post',
-      url: `${this.base}/add_results_for_cases/${this.runId}`,
-      headers: { 'Content-Type': 'application/json' },
-      auth: {
-        username: this.options.username,
-        password: this.options.password,
-      },
-      data: JSON.stringify({ results }),
-    })
-      .then(response => {
-        this.res = response;
-        );
-      })
-      .catch(error => console.error(error));
+    public deleteRun() {
+        axios({
+            method: 'post',
+            url: `${this.base}/delete_run/${this.runId}`,
+            headers: { 'Content-Type': 'application/json' },
+            auth: {
+                username: this.options.username,
+                password: this.options.password,
+            },
+        }).catch(error => console.error(error));
+    }
 
-      this.waitResponse(20000)
-      if (this.res.status == 200) {
-        console.log('\n', chalk.magenta.underline.bold('(TestRail Reporter)'));
-        console.log('\n', " - Results are published to " + chalk.magenta("https://" + this.options.domain + "/index.php?/runs/view/" + this.runId), '\n');
-      }
-  }
+    private waitResponse(delay) {
+        if (typeof this.res === "undefined" && delay > 0) {
+            deasync.sleep(1000)
+            this.waitResponse(delay - 1000)
+        }
+    }
+
+    public publishResults(results: TestRailResult[]) {
+
+        if (this.options.createTestRun == "false") {
+            this.runId = this.options.runId;
+        }
+
+        if (typeof this.runId === "undefined") {
+            console.error("runId is undefined.")
+            return
+        }
+
+        axios({
+            method: 'post',
+            url: `${this.base}/add_results_for_cases/${this.runId}`,
+            headers: { 'Content-Type': 'application/json' },
+            auth: {
+                username: this.options.username,
+                password: this.options.password,
+            },
+            data: JSON.stringify({ results }),
+        })
+            .then(response => {
+                this.res = response;
+            })
+            .catch(error => console.error(error));
+
+        this.waitResponse(20000)
+        if (this.res.status == 200) {
+            console.log('\n', chalk.magenta.underline.bold('(TestRail Reporter)'));
+            console.log('\n', " - Results are published to " + chalk.magenta("https://" + this.options.domain + "/index.php?/runs/view/" + this.runId), '\n');
+        }
+    }
 }
